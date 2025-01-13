70ef3904603b03214141016ee168fdd8271acf38
simonacorreale <simona.correale.work@gmail.com>
2025-01-13 15:16:34 +0100

    fix: labels

--- js/script.js
+++ js/script.js
@@ -6,6 +6,12 @@ Quello che devo eseguire in questo script:
 2) Scelta tipo di lavoro
 3) Verifica codice promozionale
 4) Calcolo finale
+
+Per la validazione codice:
+1) Creare una lista di codici validi.
+2) Verificare se il codice inserito è presente nella lista.
+3)Applicare lo sconto del 20% se il codice è valido.
+
 - Done
 1) Calcolo e risultato funzionante
 - To fix
@@ -18,7 +24,6 @@ const promoCode = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 // Funzione per calcolare il prezzo finale (test)
 
 function finalCalc(finalWork, hours = 10) {
-
 
     let workTime;
 
@@ -54,9 +59,9 @@ console.log(resultParagraph);
 
 // risultato 10 ore fisse
 calcButton.addEventListener('click', function (event) {
-    event.preventDefault();  // Previene l'invio del form e il ricaricamento della pagina
+    event.preventDefault();
 
-    const selectedWork = workTypeSelect.value;  // Tipo di lavoro selezionato
+    const selectedWork = workTypeSelect.value;
     const result = finalCalc(selectedWork);
     resultParagraph.textContent = `EUR: ${result}`;
 });
